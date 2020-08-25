67d1cdfcb7a5aac41dfedf5281ecd3d88e05d733
Your Name <you@example.com>
2020-08-25 15:44:40 +0700

    up

--- teknik_scripting/autogit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git add $(pwd)/.
-echo -n 'Masukan Pesan Commit : '
-read commitmsg
-git commit -m "$commitmsg"
-echo -n 'Masukan Nama Branch : '
-read branch
-git push origin $branch
--- teknik_scripting/belajar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo -n 'Masukan Nama : '
-read name
-echo "Hallo $name"
-echo 'Hallo $name'
-
-
